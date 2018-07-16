b0c904b9818e959ef1ae63ec3bf368240a8a77ab
Manzoorgithub <hassanhere8@gmail.com>
2018-07-16 22:04:24 +0530

    New Commit

---
+++ BoardworksPOMframework/BoardworksPOMframework/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+namespace BoardworksPOMframework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
